8be00e1fdf46a53638e0775cbe0301c9af0bb553
FlyinPancake <FlyinPancake@users.noreply.github.com>
2022-10-04 23:31:57 +0200

    chore: rearranged the project to differentiate between python and Arduino parts
    Added poetry for dependency management

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Colorful
+
 This script will control your cool RGB strip
+
+## Development
+
+### Python (`controller`)
+
+1. Install [poetry](https://python-poetry.org/).
+2. In the `./controller` directory run poetry install.
+3. Happy coding!
+
+### Arduino (`worker`)
+
+1. Install Arduino IDE (v1)
+<!-- 2. ... TODO -->
+3. Profit
---
+++ controller/controller/main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""Control the arduino project over serial"""
+
+import random
+import sys
+import time
+from PIL import ImageGrab  # type: ignore
+from numpy.typing import NDArray
+import numpy as np
+from serial import Serial  # type: ignore
+import serial  # type: ignore
+import keyboard  # type: ignore
+
+
+def write(message: str, serial_port: Serial) -> None:
+    """Writes a string to the serial port"""
+    serial_port.write(bytes(message, "utf-8"))
+    return
+
+
+def screenshot_to_rgb() -> NDArray:
+    """Takes an images and returns a numpy array of the rgb values"""
+    screenshot = ImageGrab.grab()  # Take the screenshot
+    return np.asarray(screenshot)
+
+
+def estabilish_handshake(ser):
+    """Estabilish a handshake with the arduino"""
+    start = time.time()
+    ser.timeout = 0.2
+    ser.flush()
+    print("beginning handshake")
+    while True:
+        magic = random.randint(0, 10000)
+        magic_str = f"H{magic}E"
+        ser.write(bytes(magic_str, "utf-8"))
+        result = "H" + ser.readall().decode("utf-8").split("H")[-1]
+        if result == magic_str:
+            break
+        elif keyboard.is_pressed("q"):
+            print("q pressed, ending handshake")
+            sys.exit(0)
+    end = time.time()
+    print(f"{end - start} seconds elapsed to estabilish handshake")
+
+
+def calculate_average_color(rgb_array: NDArray) -> NDArray:
+    """Calculates the average color of an image"""
+    return np.mean(rgb_array, axis=(0, 1))
+
+
+SAMPLE_COUNT = 5000
+VERBOSE = True
+COM_PORT = "COM3"
+BAUD_RATE = 9600
+
+if __name__ == "__main__":
+    try:
+        # arduino = serial.Serial("COM3", 9600, timeout=5)
+        with serial.Serial(COM_PORT, BAUD_RATE) as arduino:
+            estabilish_handshake(arduino)
+            arduino.timeout = 5
+
+            print("begin")
+            while True:
+                imgdata = screenshot_to_rgb()
+
+                # Calculate the dominant color
+                average = calculate_average_color(imgdata)
+
+                data_to_transmit = (
+                    f"r{int(average[0])}g{int(average[1])}b{int(average[2])}E"
+                )
+
+                write(data_to_transmit, arduino)
+                if VERBOSE:
+                    print(data_to_transmit)
+
+                read_from_arduino = arduino.readline()
+                if VERBOSE:
+                    print(read_from_arduino)
+
+                if keyboard.is_pressed("q"):
+                    print("q pressed, ending loop")
+                    break
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt, ending loop")
+        arduino.close()
+        sys.exit(0)
